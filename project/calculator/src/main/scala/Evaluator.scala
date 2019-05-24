11962374d4d28cba9ecc78e78971afd344d0f7a0
Benjamin Killeen <killeen@uchicago.edu>
2019-05-24 13:38:48 -0500

    about to change add, not really working right now

--- project/calculator/src/main/scala/Evaluator.scala
+++ project/calculator/src/main/scala/Evaluator.scala
@@ -6,8 +6,13 @@ object Evaluator {
     Empty
   }
 
-  def err (t: Term) : Term = {
+  def termErr (t: Term) : Term = {
     println(s"eval error: $t")
+    Empty
+  }
+
+  def err (msg: String) : Term = {
+    pritnln(s"eval error: %msg")
     Empty
   }
 
@@ -35,22 +40,103 @@ object Evaluator {
     case _ => castErr(tau)
   }
 
-  def negate (t: Term) : Term = t match {
+  def negate (v: Term) : Term = v match {
     case Zero => Integer(False, Zero)
     case Succ(v1) => Integer(True, Succ(v1))
     case Integer(False, v1) => Integer(True, v1)
     case Integer(True, v1) => Integer(False, v1)
     case Rational(Integer(False, v1), v2) => Rational(Integer(True, v1), v2)
     case Rational(Integer(True, v1), v2) => Rational(Integer(False, v1), v2)
-    case _ => err(t)
+    case _ => termErr(t)
   }
 
-  def positive (t:Term) : Term = t match {
+  def positive (v:Term) : Term = v match {
     case Zero => Integer(False, Zero)
     case Succ(v1) => Integer(False, Succ(v1))
     case Integer(v1, v2) => Integer(v1, v2)
     case Rational(v1, v2) => Rational(v1, v2)
-    case _ => err(t)
+    case _ => termErr(t)
+  }
+
+  def not(v: Term) : Term = v match {
+    case True => False
+    case False => True
+    case _ => err(s"Not($t)")
+  }
+
+  def even(v: Term) : Term = v match {
+    case Zero => True
+    case Succ(Zero) => False
+    case Succ(v1) => not(even(v1))
+    case Integer(_, v2) => even(v2)
+    case Rational(_, _) => err(s"Even($t)")
+    case _ => err(s"Even($t)")
+  }
+
+  def odd(t: Term) : Term = not(even(t))
+
+  // TODO: maybe don't need type for add, as long as both terms already values
+  // and properly typed, following conventions for other helpers
+  def add(t1: Term, t2: Term, tau: ty.Ty) : Term = tau match {
+      case ty.Natural => cast(this(t1), tau) match {
+        case Zero => cast(this(t2), tau)
+        case Succ(v1) => add(v1, Succ(t2), tau)
+        case _ => errMsg("Add")
+      }
+      case ty.Integer => cast(this(t1), tau) match {
+        case Integer(False | True, Zero) => cast(this(t2), tau)
+        case Integer(False, Succ(v1)) => cast(this(t2), tau) match {
+          case Integer(True | False, Zero) => Integer(False, Succ(v1))
+          case Integer(False, v2) =>
+            add(Integer(False, v1), Integer(False, Succ(v1)), tau)
+          case Integer(True, Succ(v2)) =>
+            add(Integer(False, v1), Integer(True, v2), tau)
+          case _ => errMsg("Add")
+        }
+        case Integer(True, Succ(v1)) => cast(this(t2), tau) match {
+          case Integer(True | False, Zero) => Integer(True, Succ(v1))
+          case Integer(False, Succ(v2)) =>
+            add(Integer(True, v1), Integer(False, v2), tau)
+          case Integer(True, v2) =>
+            add(Integer(True, v1), Integer(True, Succ(v2)), tau)
+          case _ => errMsg("Add")
+        }
+        case _ => errMsg("Add")
+      }
+      case ty.Rational => cast(this(t1), tau) match {
+        case Rational(Integer(False | True, Zero), _) => cast(this(t2), tau)
+        case _ => errMsg("TODO") // TODO 
+      }
+      case _ => errMsg("unchecked type")
+    }
+
+  // Quotient, Remainder
+  def divByTwoNat(v: Term) : (Term, Term) = v match {
+    case Zero => (Zero, Zero)
+    case Succ(Zero) => (Zero, Succ(Zero))
+    case Succ(Succ(v1)) =>
+      val (q, r) = divByTwoNat(v1)
+      (Succ(q), r)
+    case _ => err("divByTwoNat unchecked type")
+  }
+
+  def multiply (v1: Term, v2: Term, tau: ty.Ty) : Term = tau match {
+    // assumes same type,
+    case ty.Natural => (v1, v2) match {
+      case (Zero, _) => Zero
+      case (_, Zero) => Zero
+      case (Succ(Zero), v2) => v2
+      case (Succ(v1), v2) => v2
+    }
+    case ty.Integer =>
+    case ty.Rational =>
+  }
+
+  def gcd(v1: Term, v2: Term) : Term = if (v1 == Zero) v2
+    else (divByTwoNat(v1), divByTwoNat(v2)) match {
+    case ((q1, Zero), (q2, Zero)) => 
+    case 
+
   }
 
   def apply (t: Term) : Term = t match {
@@ -65,37 +151,6 @@ object Evaluator {
     case Rational(t1, t2) => Rational(this(t1), this(t2))
     case Negate(t1) => negate(this(t1))
     case Positive(t1) => positive(this(t1))
-    case Add(t1, t2) => t.tau match {
-      case ty.Natural => cast(this(t1), t.tau) match {
-        case Zero => cast(this(t2), t.tau)
-        case Succ(v1) => this(Add(v1, Succ(t2)).withType(t.tau))
-        case _ => err(t)
-      }
-      case ty.Integer => cast(this(t1), t.tau) match {
-        case Integer(False | True, Zero) => cast(this(t2), t.tau)
-        case Integer(False, Succ(v1)) => cast(this(t2), t.tau) match {
-          case Integer(True | False, Zero) => Integer(False, Succ(v1))
-          case Integer(False, v2) =>
-            this(Add(Integer(False, v1), Integer(False, Succ(v1))).withType(t.tau))
-          case Integer(True, Succ(v2)) =>
-            this(Add(Integer(False, v1), Integer(True, v2)).withType(t.tau))
-          case _ => err(t)
-        }
-        case Integer(True, Succ(v1)) => cast(this(t2), t.tau) match {
-          case Integer(True | False, Zero) => Integer(True, Succ(v1))
-          case Integer(False, Succ(v2)) =>
-            this(Add(Integer(True, v1), Integer(False, v2)).withType(t.tau))
-          case Integer(True, v2) =>
-            this(Add(Integer(True, v1), Integer(True, Succ(v2)).withType(t.tau)))
-          case _ => err(t)
-        }
-        case _ => err(t)
-      }
-      case ty.Rational => cast(this(t1), t.tau) match {
-        case Rational(Integer(False | True, Zero), _) => cast(this(t2), t.tau)
-        case _ => err(t) // TODO 
-      }
-      case _ => err(t)
-    }
+    case Add(t1, t2) => add(this(t1), this(t2))
   }
 }
