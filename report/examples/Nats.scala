89661148803585ca3ae37468e9a88912a5bfc1ff
Benjamin Killeen <killeen@uchicago.edu>
2019-06-01 15:23:39 -0500

    fixed nats

--- report/examples/Nats.scala
+++ report/examples/Nats.scala
@@ -1,7 +1,21 @@
-trait Nat {
+package nats
 
+trait Nat {
+  def isZero (): Boolean
+  def pred (): Nat
+  def succ (): Nat
+  def + (y: Nat): Nat = if (y.isZero) this else this.succ + y.pred
+  def - (y: Nat): Nat = if (y.isZero) this else this.pred + y.pred
 }
 
-object Zero extends Nat {
+object Z extends Nat {
+  def isZero () = true
+  def succ () = Succ(this)
+  def pred () = this
+}
 
+case class Succ(v: Nat) extends Nat {
+  def isZero () = false
+  def succ () = Succ(this)
+  def pred () = v
 }
